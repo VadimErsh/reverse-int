5a2e113aca36d9c469bed3a87048014b659f802d
VadimErsh <vadimersh2022@gmail.com>
2023-01-21 18:52:33 +0300

    fead: add test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let math = Math.abs(n)
+  let str = math.toString()
+  let rez = str.split('').reverse().join('')
+  return +rez
 }
